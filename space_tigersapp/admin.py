f7e224d0f2d862097a4a5d4fa68781b192ce0e2f
Tevin Herbert <tevin.herbert@outlook.com>
2024-10-07 16:04:41 -0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   db.sqlite3
    	modified:   space_tigers/__pycache__/__init__.cpython-312.pyc
    	modified:   space_tigers/__pycache__/settings.cpython-312.pyc
    	modified:   space_tigers/__pycache__/urls.cpython-312.pyc
    	modified:   space_tigers/__pycache__/wsgi.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/__init__.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/admin.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/apps.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/forms.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/models.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/urls.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/views.cpython-312.pyc
    	modified:   space_tigersapp/admin.py
    	new file:   space_tigersapp/migrations/0007_order_profile.py
    	modified:   space_tigersapp/migrations/__pycache__/0001_initial.cpython-312.pyc
    	modified:   space_tigersapp/migrations/__pycache__/0002_customer.cpython-312.pyc
    	modified:   space_tigersapp/migrations/__pycache__/0003_rename_first_name_customer_fname_and_more.cpython-312.pyc
    	modified:   space_tigersapp/migrations/__pycache__/__init__.cpython-312.pyc
    	modified:   space_tigersapp/models.py

--- space_tigersapp/admin.py
+++ space_tigersapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product, Customer
+from .models import Product, Customer, Profile, Order
+from django.contrib.auth.models import User  # Import User model
 
 # Register your models here.
 
@@ -11,3 +12,19 @@ class CustomerAdmin(admin.ModelAdmin):
 
 admin.site.register(Product)
 admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Profile)
+
+# Profile and User info
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email"]
+    inlines = [ProfileInline]
+
+# Unregister the old way
+admin.site.unregister(User) 
+admin.site.register(User, UserAdmin) 
