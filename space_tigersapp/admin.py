faf2175d296422dfef8761558dcedf1d13fcc730
Nickolas J. Petrone <Njpetrone@outlook.com>
2024-10-09 13:38:42 -0700

    Revert "Please enter the commit message for your changes. Lines starting"
    This reverts commit f7e224d0f2d862097a4a5d4fa68781b192ce0e2f.

--- space_tigersapp/admin.py
+++ space_tigersapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Product, Customer, Profile, Order
-from django.contrib.auth.models import User  # Import User model
+from .models import Product, Customer
 
 # Register your models here.
 
@@ -12,19 +11,3 @@ class CustomerAdmin(admin.ModelAdmin):
 
 admin.site.register(Product)
 admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Profile)
-
-# Profile and User info
-
-class ProfileInline(admin.StackedInline):
-    model = Profile
-
-class UserAdmin(admin.ModelAdmin):
-    model = User
-    field = ["username", "first_name", "last_name", "email"]
-    inlines = [ProfileInline]
-
-# Unregister the old way
-admin.site.unregister(User) 
-admin.site.register(User, UserAdmin) 
--- space_tigersapp/models.py
+++ space_tigersapp/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
 # Create your models here.
 class Product(models.Model):
@@ -25,44 +23,4 @@ class Customer(models.Model):
     avatar_url = models.CharField(max_length=2083)
     
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"    
-        
-        
-        
-        # create Cutsomer Profile
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_modfiied = models.DateTimeField(User, auto_now = True)
-    phone = models.CharField(max_length = 20, blank = True)
-    addess1 = models.CharField(max_length = 200, blank = True)
-    addess2 = models.CharField(max_length = 200, blank = True)
-    city = models.CharField(max_length = 200, blank = True)
-    state = models.CharField(max_length = 200, blank = True)
-    zipcode = models.CharField(max_length = 200, blank = True)
-    country = models.CharField(max_length = 200, blank = True)
-
-    def __str__(self):
-        return self.user.username
-
- # Create a user Profile by Default when user signs up
-
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        user_profile = Profile(user = instance)
-        user_profile.save()
-
-# Automate the profile thing
-
-post_save.connect(create_profile, sender = User)
-
-
-
-class Order(models.Model):
-    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    order_date = models.DateTimeField(auto_now_add=True)
-    quantity = models.PositiveIntegerField()
-
-    def __str__(self):
-        return f"Order {self.id} - {self.customer.name}"
+        return f"{self.first_name} {self.last_name}"
